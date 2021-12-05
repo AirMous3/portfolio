839cb2d7135d9f643f2e4810e67b2b0f0c30fe7e
AirMous3 <airmouselol@gmail.com>
2021-12-05 20:15:35 +0300

    works scroll

--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -4,8 +4,8 @@ import { SimpleSlider } from './slider/Slider';
 import s from './works.module.scss';
 
 export const Works = (): ReactElement => (
-  <section className={`${s.main} section`}>
-    <div id="projects">
+  <section id="projects" className={`${s.main} section`}>
+    <div>
       <h2 className={s.title}>MY WORKS</h2>
 
       <SimpleSlider />
