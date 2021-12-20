890414d8b11f6981bfc4a3e64f7e5c24beed6f16
AirMous3 <airmouselol@gmail.com>
2021-12-20 15:11:08 +0300

    React-reveal effects

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -8,58 +8,60 @@ import ReactSonic from './features/8.gif';
 import { Skill } from './skill/Skill';
 import s from './skills.module.scss';
 
-export const Skills = (): ReactElement => {
-  const data = [
-    {
-      title: 'React',
-      description:
-        'React is a declarative, efficient, and flexible JavaScript library for building user interfaces.' +
-        ' It lets you compose complex UIs from small and isolated pieces of code called “components”',
-      image: ReactSonic,
-    },
-    {
-      title: 'Redux',
-      description:
-        'Redux is a pattern and library for managing and updating application state, using events called "actions".' +
-        ' It serves as a centralized store for state that needs to be used across your entire application, with rules ' +
-        'ensuring that the state can only be updated in a predictable fashion.',
-      image: ReduxSonic,
-    },
-    {
-      title: 'TypeScript',
-      description:
-        'TypeScript adds additional syntax to JavaScript to support a tighter ' +
-        'integration with your editor. Catch errors early in your editor.',
-      image: TypeScriptSonic,
-    },
-    {
-      title: 'Git',
-      description:
-        'Distributed version control system that allows you to create repositories to manage ' +
-        'collaborative development and, if necessary, revert to earlier versions of the application.',
-      image: GitSonic,
-    },
-    {
-      title: 'SASS',
-      description:
-        'Sass is a stylesheet language that’s compiled to CSS. It allows you to use variables,' +
-        ' nested rules, mixins, functions, and more, all with a fully CSS-compatible syntax. ' +
-        'Sass helps keep large stylesheets well-organized and makes it easy to share design within and across projects.',
-      image: SassSonic,
-    },
-  ];
+const Zoom = require('react-reveal/Zoom');
 
-  return (
-    <section className={`${s.main} section`}>
-      <div id="skills" className="container">
-        <h2 className={s.skillsTitle}>MY SKILLS</h2>
+const data = [
+  {
+    title: 'React',
+    description:
+      'React is a declarative, efficient, and flexible JavaScript library for building user interfaces.' +
+      ' It lets you compose complex UIs from small and isolated pieces of code called “components”',
+    image: ReactSonic,
+  },
+  {
+    title: 'Redux',
+    description:
+      'Redux is a pattern and library for managing and updating application state, using events called "actions".' +
+      ' It serves as a centralized store for state that needs to be used across your entire application, with rules ' +
+      'ensuring that the state can only be updated in a predictable fashion.',
+    image: ReduxSonic,
+  },
+  {
+    title: 'TypeScript',
+    description:
+      'TypeScript adds additional syntax to JavaScript to support a tighter ' +
+      'integration with your editor. Catch errors early in your editor.',
+    image: TypeScriptSonic,
+  },
+  {
+    title: 'Git',
+    description:
+      'Distributed version control system that allows you to create repositories to manage ' +
+      'collaborative development and, if necessary, revert to earlier versions of the application.',
+    image: GitSonic,
+  },
+  {
+    title: 'SASS',
+    description:
+      'Sass is a stylesheet language that’s compiled to CSS. It allows you to use variables,' +
+      ' nested rules, mixins, functions, and more, all with a fully CSS-compatible syntax. ' +
+      'Sass helps keep large stylesheets well-organized and makes it easy to share design within and across projects.',
+    image: SassSonic,
+  },
+];
 
-        <div className={s.skills}>
-          {data.map(({ title, description, image }) => (
-            <Skill key={title} title={title} description={description} image={image} />
-          ))}
-        </div>
+export const Skills = (): ReactElement => (
+  <section className={`${s.main} section`}>
+    <div id="skills" className="container">
+      <h2 className={s.skillsTitle}>MY SKILLS</h2>
+
+      <div className={s.skills}>
+        {data.map(({ title, description, image }) => (
+          <Zoom key={title}>
+            <Skill title={title} description={description} image={image} />
+          </Zoom>
+        ))}
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
--- src/components/works/Works.tsx
+++ src/components/works/Works.tsx
@@ -3,12 +3,15 @@ import React, { ReactElement } from 'react';
 import { SimpleSlider } from './slider/Slider';
 import s from './works.module.scss';
 
+const Fade = require('react-reveal/Fade');
+
 export const Works = (): ReactElement => (
   <section id="projects" className={`${s.main} section`}>
     <div className="container">
       <h2 className={s.title}>MY WORKS</h2>
-
-      <SimpleSlider />
+      <Fade>
+        <SimpleSlider />
+      </Fade>
     </div>
   </section>
 );
